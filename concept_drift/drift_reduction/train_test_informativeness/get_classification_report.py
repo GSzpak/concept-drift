a64e16b9c3df2feb071e53795b4aafa72f15ed77
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-11-13 16:51:52 +0100

    Added thresholds to test

--- concept_drift/drift_reduction/train_test_informativeness/get_classification_report.py
+++ concept_drift/drift_reduction/train_test_informativeness/get_classification_report.py
@@ -4,7 +4,6 @@ import click
 import pandas as pd
 
 
-# TODO: fill this
 from concept_drift.classifier.perform_classification import ClassifierFactory, perform_classification
 from concept_drift.drift_reduction.train_test_informativeness.train_test_informativeness import \
     get_drift_informativeness
@@ -13,7 +12,13 @@ from concept_drift.score_calculator.score_calculation import get_labels_from_fil
 
 # First - classification lower bound
 # Second - drift upper bound
-THRESHOLD_PAIRS_TO_TEST = []
+THRESHOLD_PAIRS_TO_TEST = [
+    (0.0, 0.05), (0.0, 0.1), (0.0, 0.2),
+    (0.25, 0.05), (0.25, 0.1), (0.25, 0.2),
+    (0.5, 0.05), (0.5, 0.1), (0.5, 0.2), (0.5, 0.4),
+    (1.0, 0.1), (1.0, 0.3), (1.0, 0.5),
+    (1.5, 0.7),
+]
 
 
 def get_columns_to_drop(classif_info, drift_info, classif_lower_bound, drift_upper_bound):
@@ -26,7 +31,12 @@ def get_columns_to_drop(classif_info, drift_info, classif_lower_bound, drift_upp
 
 def get_classification_report(training_data, training_labels, test_data, test_labels,
                               classif_info, drift_info, classif_lower_bound, drift_upper_bound):
+    assert training_data.shape[1] == test_data.shape[1]
+    num_of_columns = training_data.shape[1]
+    print 'Calculating report for thresholds: {}, {}'.format(classif_lower_bound, drift_upper_bound)
     columns_to_drop = get_columns_to_drop(classif_info, drift_info, classif_lower_bound, drift_upper_bound)
+    features_selected = num_of_columns - len(columns_to_drop)
+    print '{} features selected'.format(features_selected)
     training_data_prepared = training_data.drop(training_data.columns[columns_to_drop], axis=1, inplace=False)
     test_data_prepared = test_data.drop(test_data.columns[columns_to_drop], axis=1, inplace=False)
     report_rows = []
@@ -38,7 +48,9 @@ def get_classification_report(training_data, training_labels, test_data, test_la
             test_labels,
             classifier_name
         )
-        report_rows.append([classif_lower_bound, drift_upper_bound, classifier_name, train_score, test_score])
+        print '{}: train score {}, test score {}'.format(classifier_name, train_score, test_score)
+        report_rows.append([classif_lower_bound, drift_upper_bound, classifier_name,
+                            features_selected, train_score, test_score])
     return report_rows
 
 
@@ -57,6 +69,14 @@ def main(training_data_path, training_labels_path, test_data_path, test_labels_p
     drift_info = get_drift_informativeness(training_data, test_data)
     with open(report_file_path, 'w') as report_file:
         report_writer = csv.writer(report_file)
+        report_writer.writerow([
+            'classification lower bound',
+            'drift upper bound',
+            'classifier name',
+            'number of features',
+            'train score',
+            'test score'
+        ])
         for classif_lower_bound, drift_upper_bound in THRESHOLD_PAIRS_TO_TEST:
             report_rows = get_classification_report(
                 training_data,
