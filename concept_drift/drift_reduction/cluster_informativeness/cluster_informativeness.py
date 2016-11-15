1dea71f010c9ff8502304f54623672ed03ec45f6
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-11-15 15:28:41 +0100

    Second version of clustering drift detection

--- concept_drift/drift_reduction/cluster_informativeness/cluster_informativeness.py
+++ concept_drift/drift_reduction/cluster_informativeness/cluster_informativeness.py
@@ -2,6 +2,7 @@ import csv
 from collections import defaultdict
 import os
 
+import numpy as np
 from sklearn.cluster import KMeans
 
 from concept_drift.drift_reduction.feature_informativeness_measures import get_feature_informativeness_measure
@@ -9,62 +10,128 @@ from concept_drift.drift_reduction.utils import read_info_from_cache_file, write
 from concept_drift.score_calculator.score_calculation import get_labels_from_file
 from settings import SEED
 
-CACHE_FILE_TEMPLATE = os.path.join(CACHE_DIR, 'cluster_informativeness_{}.csv')
-CLUSTER_LABELS_FILE = os.path.join(CACHE_DIR, 'cluster_labels.csv')
-# We have 4 firefighters
-NUM_OF_CLUSTERS = 4
+
+class ClusterInformativenessCalculator(object):
+
+    # We have 4 firefighters
+    NUM_OF_CLUSTERS = 4
+
+    @staticmethod
+    def group_by_label(y):
+        indices_for_label = defaultdict(list)
+        for index, label in enumerate(y):
+            indices_for_label[label].append(index)
+        return indices_for_label
+
+    def make_cache_file_name(self, measure_name):
+        raise NotImplementedError
+
+    def cluster(self, X):
+        clustering = KMeans(
+            n_clusters=self.NUM_OF_CLUSTERS,
+            max_iter=1000,
+            n_init=20,
+            tol=0.0000001,
+            verbose=0,
+            copy_x=False,
+            n_jobs=1,
+            random_state=SEED
+        )
+        return clustering.fit_predict(X)
+
+    def _get_cluster_labels(self, X, indices):
+        X_copy = X.iloc[indices, :]
+        assert X_copy.shape == (len(indices), X.shape[1])
+        return self.cluster(X_copy)
+
+    def _calculate_drift_informativeness(self, X, X_for_clustering, indices_for_label, informativeness_measure):
+        raise NotImplementedError
+
+    def get_cluster_drift_informativeness(self, X, X_for_clustering, y,
+                                          informativeness_measure_name='mutual_info', use_cache=True):
+        cache_file_name = self.make_cache_file_name(informativeness_measure_name)
+        if use_cache and os.path.isfile(cache_file_name):
+            return read_info_from_cache_file(cache_file_name)
+        informativeness_measure = get_feature_informativeness_measure(informativeness_measure_name)
+        indices_for_label = self.group_by_label(y)
+        informativeness = self._calculate_drift_informativeness(
+            X,
+            X_for_clustering,
+            indices_for_label,
+            informativeness_measure
+        )
+        write_info_to_cache_file(cache_file_name, informativeness)
+        return informativeness
 
 
-def write_labels_to_file(file_path, labels):
-    with open(file_path, 'w') as f:
-        writer = csv.writer(f)
-        for label in labels:
-            writer.writerow([label])
+class ClusterInformativenessCalculatorV1(ClusterInformativenessCalculator):
+
+    _CACHE_FILE_TEMPLATE = os.path.join(CACHE_DIR, 'cluster_informativeness_v1_{}.csv')
+    _CLUSTER_LABELS_FILE = os.path.join(CACHE_DIR, 'cluster_labels.csv')
+
+    @staticmethod
+    def write_labels_to_file(file_path, labels):
+        with open(file_path, 'w') as f:
+            writer = csv.writer(f)
+            for label in labels:
+                writer.writerow([label])
+
+    def make_cache_file_name(self, measure_name):
+        return self._CACHE_FILE_TEMPLATE.format(measure_name)
+
+    def _get_cluster_labels(self, X_for_clustering, indices_for_label):
+        if os.path.isfile(self._CLUSTER_LABELS_FILE):
+            return get_labels_from_file(self._CLUSTER_LABELS_FILE)
+        labels_after_clustering = [0 for _ in xrange(len(y))]
+        for base_label, indices in indices_for_label.iteritems():
+            cluster_labels = self._get_cluster_labels(X_for_clustering, indices)
+            for index, cluster_label in zip(indices, cluster_labels):
+                final_label = '{}_{}'.format(base_label, cluster_label)
+                labels_after_clustering[index] = final_label
+        self.write_labels_to_file(self._CLUSTER_LABELS_FILE, labels_after_clustering)
+        return labels_after_clustering
+
+    def _calculate_drift_informativeness(self, X, X_for_clustering, indices_for_label, informativeness_measure):
+        y_cluster = self._get_cluster_labels(X_for_clustering, indices_for_label)
+        return informativeness_measure(X, y_cluster)
 
 
-def cluster(X):
-    clustering = KMeans(
-        n_clusters=NUM_OF_CLUSTERS,
-        max_iter=1000,
-        tol=0.0000001,
-        verbose=0,
-        copy_x=False,
-        n_jobs=1,
-        random_state=SEED
-    )
-    return clustering.fit_predict(X)
+class ClusterInformativenessCalculatorV2(ClusterInformativenessCalculator):
 
+    _CACHE_FILE_TEMPLATE = os.path.join(CACHE_DIR, 'cluster_informativeness_v2_{}.csv')
+    _PARTIAL_RESULTS_CACHE_DIR = os.path.join(CACHE_DIR, 'cluster_v2_partial_results')
+    if not os.path.exists(_PARTIAL_RESULTS_CACHE_DIR):
+        os.makedirs(_PARTIAL_RESULTS_CACHE_DIR)
 
-def group_by_label(y):
-    indices_for_label = defaultdict(list)
-    for index, label in enumerate(y):
-        indices_for_label[label].append(index)
-    return indices_for_label
+    def make_cache_file_name(self, measure_name):
+        return self._CACHE_FILE_TEMPLATE.format(measure_name)
 
+    def _make_partial_results_cache_name(self, label, measure_name):
+        return os.path.join(self._PARTIAL_RESULTS_CACHE_DIR, '{}_{}.csv'.format(label, measure_name))
 
-def get_cluster_labels(X_for_clustering, y, use_cache=True):
-    if use_cache and os.path.isfile(CLUSTER_LABELS_FILE):
-        return get_labels_from_file(CLUSTER_LABELS_FILE)
-    indices_for_label = group_by_label(y)
-    labels_after_clustering = [0 for _ in xrange(len(y))]
-    for base_label, indices in indices_for_label.iteritems():
-        X_copy = X_for_clustering.iloc[indices, :]
-        assert X_copy.shape == (len(indices), X_for_clustering.shape[1])
-        cluster_labels = cluster(X_copy)
-        for index, cluster_label in zip(indices, cluster_labels):
-            final_label = '{}_{}'.format(base_label, cluster_label)
-            labels_after_clustering[index] = final_label
-    write_labels_to_file(CLUSTER_LABELS_FILE, labels_after_clustering)
-    return labels_after_clustering
+    def _get_informativeness_for_label(self, X, X_for_clustering, label, indices, informativeness_measure):
+        cache_file_name = self._make_partial_results_cache_name(label, informativeness_measure.__name__)
+        if os.path.isfile(cache_file_name):
+            return read_info_from_cache_file(cache_file_name)
+        cluster_labels = self._get_cluster_labels(X_for_clustering, indices)
+        X_part = X.iloc[indices, :]
+        label_informativeness = informativeness_measure(X_part, cluster_labels)
+        write_info_to_cache_file(cache_file_name, label_informativeness)
+        return label_informativeness
 
-
-def get_cluster_drift_informativeness(X, X_for_clustering, y,
-                                      informativeness_measure_name='mutual_info', use_cache=True):
-    cache_file_name = CACHE_FILE_TEMPLATE.format(informativeness_measure_name)
-    if use_cache and os.path.isfile(cache_file_name):
-        return read_info_from_cache_file(cache_file_name)
-    y_cluster = get_cluster_labels(X_for_clustering, y, use_cache=use_cache)
-    informativeness_measure = get_feature_informativeness_measure(informativeness_measure_name)
-    informativeness = informativeness_measure(X, y_cluster)
-    write_info_to_cache_file(cache_file_name, informativeness)
-    return informativeness
+    def _calculate_drift_informativeness(self, X, X_for_clustering, indices_for_label, informativeness_measure):
+        results = []
+        for label, indices in indices_for_label.iteritems():
+            print 'Calculating informativeness for label {}, {} instances'.format(label, len(indices))
+            partial_informativeness = self._get_informativeness_for_label(
+                X,
+                X_for_clustering,
+                label,
+                indices,
+                informativeness_measure
+            )
+            results.append(partial_informativeness)
+        assert len(results) == len(indices_for_label)
+        result = np.mean(results, axis=0)
+        assert len(result) == X.shape[1]
+        return result
--- concept_drift/drift_reduction/cluster_informativeness/plot_informativeness.py
+++ concept_drift/drift_reduction/cluster_informativeness/plot_informativeness.py
@@ -2,17 +2,24 @@ import click
 import pandas as pd
 
 from concept_drift.drift_reduction.cluster_informativeness.cluster_informativeness import \
-    get_cluster_drift_informativeness
+    ClusterInformativenessCalculatorV1, ClusterInformativenessCalculatorV2
 from concept_drift.drift_reduction.utils import get_classification_informativeness, plot_informativeness
 from concept_drift.score_calculator.score_calculation import get_labels_from_file
+
+
+_VERSION_TO_CLASS = {
+    1: ClusterInformativenessCalculatorV1(),
+    2: ClusterInformativenessCalculatorV2()
+}
 
 
 @click.command()
 @click.argument('training-data-path', type=click.Path(exists=True, dir_okay=False))
 @click.argument('training-labels-path', type=click.Path(exists=True, dir_okay=False))
 @click.argument('data-for-clustering-path', type=click.Path(exists=True, dir_okay=False))
+@click.option('--version', '-v', type=click.INT, default=1)
 @click.option('--info-measure-name', '-i', type=click.STRING, default='mutual_info')
-def main(training_data_path, training_labels_path, data_for_clustering_path, info_measure_name):
+def main(training_data_path, training_labels_path, data_for_clustering_path, version, info_measure_name):
     training_data = pd.read_csv(training_data_path, header=None, dtype='float32')
     training_labels = get_labels_from_file(training_labels_path)
     training_data_for_clustering = pd.read_csv(data_for_clustering_path, header=None, dtype='float32')
@@ -21,7 +28,8 @@ def main(training_data_path, training_labels_path, data_for_clustering_path, inf
         training_labels,
         informativeness_measure_name=info_measure_name
     )
-    y = get_cluster_drift_informativeness(
+    info_calculator = _VERSION_TO_CLASS[version]
+    y = info_calculator.get_cluster_drift_informativeness(
         training_data,
         training_data_for_clustering,
         training_labels,
