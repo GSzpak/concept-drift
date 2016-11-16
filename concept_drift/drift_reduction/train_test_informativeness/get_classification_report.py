ae60ca1d85fa0f130ec34bfd5891cf5b054266fb
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-11-16 18:15:58 +0100

    Obtaining full report for train-test informativeness

--- concept_drift/drift_reduction/train_test_informativeness/get_classification_report.py
+++ concept_drift/drift_reduction/train_test_informativeness/get_classification_report.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 import click
 import pandas as pd
@@ -10,6 +11,8 @@ from concept_drift.score_calculator.score_calculation import get_labels_from_fil
 
 # First - classification lower bound
 # Second - drift upper bound
+from settings import THESIS_DATA_DIR
+
 THRESHOLD_PAIRS_TO_TEST = {
     'mutual_info': [
         (0.0, 0.7), (0.0, 0.05), (0.0, 0.1), (0.0, 0.2),
@@ -37,51 +40,54 @@ THRESHOLD_PAIRS_TO_TEST = {
 }
 
 
+def _make_report_path(info_measure_name):
+    return os.path.join(THESIS_DATA_DIR, 'train_test_{}_report.csv'.format(info_measure_name))
+
+
 @click.command()
 @click.argument('training-data-path', type=click.Path(exists=True, dir_okay=False))
 @click.argument('training-labels-path', type=click.Path(exists=True, dir_okay=False))
 @click.argument('test-data-path', type=click.Path(exists=True, dir_okay=False))
 @click.argument('test-labels-path', type=click.Path(exists=True, dir_okay=False))
-@click.argument('report-file-path', type=click.Path(exists=False, dir_okay=False))
-@click.option('--info-measure-name', '-i', type=click.STRING, default='mutual_info')
-def main(training_data_path, training_labels_path, test_data_path, test_labels_path, report_file_path, info_measure_name):
+def main(training_data_path, training_labels_path, test_data_path, test_labels_path):
     training_data = pd.read_csv(training_data_path, header=None, dtype='float32')
     training_labels = get_labels_from_file(training_labels_path)
     test_data = pd.read_csv(test_data_path, header=None, dtype='float32')
     test_labels = get_labels_from_file(test_labels_path)
-    classif_info = get_classification_informativeness(
-        training_data,
-        training_labels,
-        informativeness_measure_name=info_measure_name
-    )
-    drift_info = get_drift_informativeness(
-        training_data,
-        test_data,
-        informativeness_measure_name=info_measure_name
-    )
-    pairs_to_test = THRESHOLD_PAIRS_TO_TEST[info_measure_name]
-    with open(report_file_path, 'w') as report_file:
-        report_writer = csv.writer(report_file)
-        report_writer.writerow([
-            'classification lower bound',
-            'drift upper bound',
-            'classifier name',
-            'number of features',
-            'train score',
-            'test score'
-        ])
-        for classif_lower_bound, drift_upper_bound in pairs_to_test:
-            report_rows = get_classification_report(
-                training_data,
-                training_labels,
-                test_data,
-                test_labels,
-                classif_info,
-                drift_info,
-                classif_lower_bound,
-                drift_upper_bound
-            )
-            report_writer.writerows(report_rows)
+    for info_measure_name, threshold_pairs_to_test in THRESHOLD_PAIRS_TO_TEST.iteritems():
+        classif_info = get_classification_informativeness(
+            training_data,
+            training_labels,
+            informativeness_measure_name=info_measure_name
+        )
+        drift_info = get_drift_informativeness(
+            training_data,
+            test_data,
+            informativeness_measure_name=info_measure_name
+        )
+        report_file_path = _make_report_path(info_measure_name)
+        with open(report_file_path, 'w') as report_file:
+            report_writer = csv.writer(report_file)
+            report_writer.writerow([
+                'classification lower bound',
+                'drift upper bound',
+                'classifier name',
+                'number of features',
+                'train score',
+                'test score'
+            ])
+            for classif_lower_bound, drift_upper_bound in threshold_pairs_to_test:
+                report_rows = get_classification_report(
+                    training_data,
+                    training_labels,
+                    test_data,
+                    test_labels,
+                    classif_info,
+                    drift_info,
+                    classif_lower_bound,
+                    drift_upper_bound
+                )
+                report_writer.writerows(report_rows)
 
 
 if __name__ == '__main__':
