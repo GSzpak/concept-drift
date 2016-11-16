5858b9098d6ffd162584b91069a0b45dd38df54a
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-11-16 15:30:41 +0100

    Bugfix in integral calculation

--- concept_drift/feature_extraction/time_related_features.py
+++ concept_drift/feature_extraction/time_related_features.py
@@ -9,8 +9,13 @@ def derivative(time_series, time):
 
 def integral(time_series, time):
     assert len(time_series) == len(time)
-    return [((time_series[i] + time_series[i - 1]) * (time[i] - time[i - 1])) / 2.
-            for i in xrange(1, len(time_series))]
+    result = []
+    current_integral = 0
+    for i in xrange(1, len(time_series)):
+        partial_integral = ((time_series[i] + time_series[i - 1]) * (time[i] - time[i - 1])) / 2.
+        current_integral += partial_integral
+        result.append(current_integral)
+    return result
 
 
 def get_time_related_features(time_series, time):
